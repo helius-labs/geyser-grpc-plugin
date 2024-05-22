d086376d18cac717c82cf1dd9b9e17e6f4ce3971
vovkman <liam.vovk@gmail.com>
2024-05-22 09:59:38 -1000

    fix endpoint config

--- client/src/interceptor.rs
+++ client/src/interceptor.rs
@@ -9,7 +9,7 @@ impl Interceptor for GrpcInterceptor {
     fn call(&mut self, mut request: tonic::Request<()>) -> Result<tonic::Request<()>, Status> {
         request
             .metadata_mut()
-            .insert("access-token", self.access_token.parse().unwrap());
+            .insert("x-token", self.access_token.parse().unwrap());
         Ok(request)
     }
 }
